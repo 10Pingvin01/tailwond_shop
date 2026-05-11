4131f1d6e988fbebf8a89aa1d46b0b4965e48822
Кирилл Куликов <ikylikov2004k@gmail.com>
2026-05-11 12:19:45 +0300

    changes

--- webpack.config.js
+++ webpack.config.js
@@ -1,16 +1,16 @@
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const path = require('path');
 
-module.exports = {
+module.exports = (_,argv = {}) =>({
     entry: './src/index.js',
-    mode: 'development',
+    mode: argv.mode ||'development',
     plugins: [new HtmlWebpackPlugin({
         'template': './src/index.html',
     })],
     devServer: {
         static: {
             directory: path.resolve(__dirname, 'public'),
-            publicPath: '/',
+            publicPath: '/public',
         },
     },
     module: {
@@ -36,4 +36,4 @@ module.exports = {
             },
         ],
     },
-};
+});
